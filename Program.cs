1da126cd95682b681fb5d1a3bda930061c127895
AzizDeveloper04 <saparbaev0131@gmail.com>
2023-08-26 16:00:21 +0500

    Answers: Ketma-ketlik

--- Program.cs
+++ Program.cs
@@ -1,48 +1,20 @@
-﻿﻿// 1
-   System.Console.WriteLine("enter message: ");
-   string text=Console.ReadLine();
-   System.Console.WriteLine("enter number");
-   int n=Convert.ToInt32(Console.ReadLine());
-   string answer= n>text.Length ? text.ToUpper():text.ToLower();
-   System.Console.WriteLine(answer);
+﻿// 1
+using System.Diagnostics;
 
-// 2
-System.Console.WriteLine("x=");
-int x=Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("y=");
-int y=Convert.ToInt32(Console.ReadLine());
-string result="";
-if(x>y)
-{
-  result="x>y";
-}
-else 
-if(x<y)
-{
-    result="y>x";
-}
-else if(x==y)
-{
-   result="x=y";
-}
-else
-{
-    result="x and y are not comparable!!!";
-}
-System.Console.WriteLine(result);
+decimal r=Convert.ToDecimal(Console.ReadLine());
+decimal s=r*r*(decimal)Math.PI;
+System.Console.WriteLine(s);
 
-// 3
-System.Console.WriteLine("Hafta kunini kiriting: ");
-string day=Console.ReadLine();
-string translateday=day switch
-{
-      "dushanba" => "monday",
-      "seshanba" => "tuesday",
-    "chorshanba" => "wednesday",
-     "payshanba" => "thursday",
-          "juma" => "firday",
-        "shanba" => "saturday",
-      "yakshaba" => "sunday",
-               _ => "the day not found"
-};
-System.Console.WriteLine(translateday);
+//2
+Console.Write("Enter the amount of your money: ");
+int sum=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("enter the course: ");
+int course=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"your money is worth {sum*course}");
+
+//3
+System.Console.WriteLine("enter your birth year: ");
+int birth=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("enter the current year: ");
+int now=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"you have lived {(now-birth)*365} days so far");
