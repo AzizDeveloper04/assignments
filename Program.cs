888bc2f8d3e15bef985e0468fd439a6f0c4a4f29
AzizDeveloper04 <saparbaev0131@gmail.com>
2023-08-26 11:56:44 +0500

    Basics: Upload project

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿// 1
+//    System.Console.WriteLine("enter message: ");
+//    string text=Console.ReadLine();
+//    System.Console.WriteLine("enter number");
+//    int n=Convert.ToInt32(Console.ReadLine());
+//    string answer= n>text.Length ? text.ToUpper():text.ToLower();
+//    System.Console.WriteLine(answer);
+
+// // 2
+// int x=Convert.ToInt32(Console.ReadLine());
+// int y=Convert.ToInt32(Console.ReadLine());
+// if(x>y)
+// {
+//   System.Console.WriteLine("x>y");
+// }
+// else 
+// if(x<y)
+// {
+//     System.Console.WriteLine("y>x");
+// }
+// else if(x==y)
+// {
+//     System.Console.WriteLine("x=y");
+// }
+// else
+// {
+//     System.Console.WriteLine("x and y are not comparable!!!");
+// }
+
+// 3
+// System.Console.WriteLine("Hafta kunini kiriting: ");
+// string day=Console.ReadLine();
+// string translateday=day switch
+// {
+//       "dushanba" => "monday",
+//       "seshanba" => "tuesday",
+//     "chorshanba" => "wednesday",
+//      "payshanba" => "thursday",
+//           "juma" => "firday",
+//         "shanba" => "saturday",
+//       "yakshaba" => "sunday",
+//                _ => "the day not found"
+// };
+// System.Console.WriteLine(translateday);
+
+
