e5c9a8328951f6afa668e05c08e6cd4eed0504f4
AzizDeveloper04 <saparbaev0131@gmail.com>
2023-08-27 11:48:07 +0500

    Takrorlash:answer

--- Program.cs
+++ Program.cs
@@ -1,19 +1,42 @@
-﻿// 1
+﻿int sum=0,cikl=1;
+while(cikl<1000)
+{
+   sum+=cikl;
+   cikl+=2;
+}
+System.Console.WriteLine(sum);
 
-decimal r=Convert.ToDecimal(Console.ReadLine());
-decimal s=r*r*(decimal)Math.PI;
-System.Console.WriteLine(s);
+// 2
 
-//2
-Console.Write("Enter the amount of your money: ");
-int sum=Convert.ToInt32(Console.ReadLine());
-System.Console.Write("enter the course: ");
-int course=Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"your money is worth {sum*course}");
+int n=Convert.ToInt32(Console.ReadLine());
+int[] array=new int[n];
+for(int i=0;i<n;i++)
+{
+    System.Console.Write($"array[{i}]=");
+    array[i]=Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine();
+}
+int max=array[0],min=array[0]; 
+for(int i=1;i<n;i++)
+{
+ if(min>array[i]) 
+ {
+     min=array[i];
+ }
+ if(max<array[i])
+ {
+    max=array[i];
+ }
+}
+System.Console.WriteLine($"arraydagi maximal va minimal sonlar kopaytmasi : {max*min}");
 
-//3
-System.Console.WriteLine("enter your birth year: ");
-int birth=Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("enter the current year: ");
-int now=Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"you have lived {(now-birth)*365} days so far");
+// 3
+uint x=Convert.ToUInt32(Console.ReadLine());
+uint sum1=1;
+System.Console.Write($"{x}!");
+while(x>=1)
+{
+    sum1*=x;
+    x--;     
+}
+System.Console.Write($"={sum1}");
